b72a1adc1d1444a8915d1fd27bfde81ff5880650
YASH <yashsanjay.k21@iiits.in>
2024-03-05 19:56:15 +0530

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/sign-in' element={<SignIn/>} />
@@ -17,6 +19,7 @@ export default function App() {
         <Route path='/about'  element={<About/>}/>
         <Route path='/profile' element={<Profile/>} />
       </Routes>
+      
     </BrowserRouter>
   )
 }
