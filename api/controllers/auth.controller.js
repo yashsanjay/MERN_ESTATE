608d8ade5d2f1dc0fb5fd51c47565206a143dba0
YASH <yashsanjay.k21@iiits.in>
2024-03-06 14:09:49 +0530

    create signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+import e from "express";
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+export const signup=async(req,res)=>{
+    const {username,email,password}=req.body;
+    const hashedPassword=bcryptjs.hashSync(password,10);
+    const newUser = new User({ username:username,email:email,password:hashedPassword });
+    try {
+        await  newUser.save();
+        res.status(201).json("user created successfully!");
+        
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
+}
