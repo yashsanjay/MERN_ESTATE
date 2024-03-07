d9bbaf01f8b695680ad6c100b622fef539201ed5
YASH <yashsanjay.k21@iiits.in>
2024-03-07 23:27:42 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import express from "express";
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
@@ -9,6 +11,25 @@ export const signup=async(req,res,next)=>{
         await  newUser.save();
         res.status(201).json("user created successfully!");
         
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const validUser= await User.findOne({email:email});
+        if (!validUser) {
+            return next(errorHandler(404,"user not found!!"));
+        }
+        const validPassword= bcryptjs.compareSync(password,validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401,"Wrong credentials!!"))
+        }
+        const {password:pass,...rest}=validUser._doc;
+        const token= jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
     } catch (error) {
         next(error);
     }
