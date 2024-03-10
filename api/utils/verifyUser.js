7db5c90a2841442f3c69e194eaf44debd98b0ca1
YASH <yashsanjay.k21@iiits.in>
2024-03-11 04:15:42 +0530

    add delete user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
 import { errorHandler } from "./error.js";
 export const verifyToken=(req,res,next)=>{
-    
+
     const token=req.cookies.access_token;
+
     if (!token) {
         return next(errorHandler(401,"unauthorized"));
     }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,6 +12,9 @@ import {
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -81,6 +84,26 @@ export default function Profile() {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      console.log("heyyy");
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      console.log("heyyysh");
+      const data = await res.json();
+      console.log("heyyysh");
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      console.log(error);
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -142,7 +165,12 @@ export default function Profile() {
         </button>
       </form>
       <div className="flex justify-between mt-4">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span
+          className="text-red-700 cursor-pointer"
+          onClick={handleDeleteUser}
+        >
+          Delete Account
+        </span>
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
