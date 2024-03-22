53e63bd0859ce2feb9464ab7c1568d1e29d4c6ad
YASH <yashsanjay.k21@iiits.in>
2024-03-22 23:48:25 +0530

    complete header searchform functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,36 +1,75 @@
-import React from 'react'
-import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import {useSelector} from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-  const {currentUser}=useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const term = urlParams.get("searchTerm");
+    if (term) {
+      setSearchTerm(term);
+    }
+  }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-lg '>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-      <Link to='/'>
-     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-      <span className='text-slate-500'>Taj</span>
-      <span className='text-slate-700'>Estate</span>
-     </h1>
-     </Link>
-     <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-      <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-      <FaSearch className='text-slate-600 '/>
-     </form>
-     <ul className='flex gap-4'>
-      <Link to='/'>
-      <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
-      </Link>
-      <Link to='/about'>
-      <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
-      </Link>
-      <Link to='/profile'>
-        {currentUser ? (<img src={currentUser.avtar} className='rounded-full h-8 w-8 object-cover' alt='profile'/>):<li className=' text-slate-700 hover:underline'>Sign-in</li>}
-      
-      </Link>
-     </ul>
+    <header className="bg-slate-200 shadow-lg ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Taj</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className="text-slate-600 " />
+          </button>
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avtar}
+                className="rounded-full h-8 w-8 object-cover"
+                alt="profile"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline">Sign-in</li>
+            )}
+          </Link>
+        </ul>
       </div>
     </header>
-  )
+  );
 }
