47742bb91c248de0ef572a50fa48e391f100bc15
YASH <yashsanjay.k21@iiits.in>
2024-03-22 23:26:24 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing,deleteListing,updateListing,getListings } from '../controllers/listing.controller.js';
+import { createListing,deleteListing,updateListing,getListings,getListingss } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router.post("/create",verifyToken,createListing);
 router.delete("/delete/:id",verifyToken,deleteListing);
 router.post("/update/:id",verifyToken,updateListing);
 router.get("/get/:id",getListings);
+router.get("/get",getListingss);
 
 export default router;
