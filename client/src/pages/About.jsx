dd465d2aa7100138eae702935dd4433bc8fc6821
YASH <yashsanjay.k21@iiits.in>
2024-03-25 01:42:55 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div >
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Taj Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to TajEstate, your premier destination for all things real
+        estate. At TajEstate, we understand that finding the perfect property is
+        more than just a transaction; it's about finding your dream home, your
+        ideal investment, or your next business venture. With years of
+        experience and a passion for connecting people with their perfect
+        properties, we pride ourselves on offering unparalleled service and
+        expertise to our clients. Whether you're buying, selling, or renting,
+        our dedicated team is here to guide you every step of the way, ensuring
+        a seamless and rewarding experience.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        At TajEstate, we believe in the power of personalized service. Our team
+        of skilled professionals takes the time to understand your unique needs
+        and goals, working tirelessly to match you with properties that exceed
+        your expectations. From luxury estates to cozy starter homes, commercial
+        spaces to vacation rentals, we have an extensive portfolio to cater to
+        every taste and budget. With a commitment to integrity, transparency,
+        and professionalism, we strive to build lasting relationships with our
+        clients, earning their trust and loyalty through our unwavering
+        dedication to excellence.
+      </p>
+
+      <p className="mb-4 text-slate-700">
+        Beyond just facilitating transactions, TajEstate is dedicated to being a
+        valuable resource for all things real estate. Through our informative
+        blog, industry insights, and expert advice, we aim to empower our
+        clients with the knowledge and tools they need to make informed
+        decisions. Whether you're a first-time homebuyer navigating the
+        complexities of the market or a seasoned investor seeking new
+        opportunities, TajEstate is here to support you on your real estate
+        journey. Welcome to TajEstate, where your real estate dreams become a
+        reality.
+      </p>
     </div>
-  )
+  );
 }
