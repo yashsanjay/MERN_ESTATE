5b9bf8904dce97f390b224b8d076b97a56836f04
YASH <yashsanjay.k21@iiits.in>
2024-07-20 14:16:07 +0530

    add pre installation svd

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -14,6 +15,8 @@ console.log(err);
 })
 
 const app=express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -26,6 +29,12 @@ app.listen(3000,()=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
